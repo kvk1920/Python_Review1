c33bdafa444e691e42577ed6b3edee98c21238c4
kvk1920 <kvk1901@yandex.ru>
2018-03-22 20:32:52 +0300

    Fixed many problems with file names.

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import os
 
+
 def create_parser():
     """Create command line parser.
 
@@ -10,19 +11,24 @@ def create_parser():
     """
     parser = argparse.ArgumentParser(
         prog="Script for collecting statistics to generation new text.",
-        description= '''This program collects statistic on the source texts.''',
-        epilog= '''(c) March 2018, Kalmykov V.K.'''
+        description='''This program collects statistic on the source
+        texts.''',
+        epilog='''(c) March 2018, Kalmykov V.K.'''
     )
-    parser.add_argument('--input-dir', help= "Path to source directory", nargs= '?')
-    parser.add_argument('--model', help= 'Path to result directory', nargs= 1)
-    parser.add_argument('--lc', help= 'Save texts in lowercase.', nargs= '?')
+    parser.add_argument('--input-dir', help="Path to source directory",
+                        type=str, nargs='?')
+    parser.add_argument('--model', help='Path to result directory',
+                        nargs=1,
+                        type=str)
+    parser.add_argument('--lc', help= 'Save texts in lowercase.')
     return parser
 
 
 parser = create_parser()
 commands = parser.parse_args()
 
-sys.stdout = commands.model
+result_file = open(str(*commands.model), "w")
+
 
 def get_filelist():
     """
@@ -34,12 +40,13 @@ def get_filelist():
     filelist = list()
     filelist.append(sys.stdin)
 
-    if commands.input_dir != None:
-        filenames = os.listdir()
+    if commands.input_dir:
+        filenames = os.listdir(commands.input_dir)
         filelist.clear()
         for filename in filenames:
-            filelist.append(open(filename, "r"))
+            filelist.append(open(commands.input_dir + filename, "r"))
     return filelist
+
 
 def prepare_line(line):
     """
@@ -54,9 +61,8 @@ def prepare_line(line):
     for char in line:
         if char.isalpha():
             good_line += char
-        else:
+        elif good_line and good_line[-1] != ' ':
             good_line += ' '
-
     return list(good_line.split())
 
 def write_model(model_to_write):
@@ -67,10 +73,13 @@ def write_model(model_to_write):
     :param model_to_write: Model to write.
     """
     for first_word in model_to_write.keys():
-        print(first_word, *model_to_write[first_word])
+        result_file.write(first_word + " " +
+                          " ".join(model_to_write[first_word]) + "\n")
 
-list_of_files = get_filelist()  # This list includes only stdin if user wants read from stdin
-model = dict()                  # Here's a model of input text.
+
+list_of_files = get_filelist()
+model = dict()
+
 
 def add_pair(first_word, second_word):
     """
@@ -85,14 +94,16 @@ def add_pair(first_word, second_word):
     else:
         model[first_word] = list(second_word)
 
+
 for file in list_of_files:
     line = file.readline()
     prev_word = None
-    while not line == "":
+    while line != "":
         word_list = prepare_line(line)
         for word in word_list:
-            if prev_word != None:
+            if prev_word:
                 add_pair(prev_word, word)
             prev_word = word
+        line = file.readline()
 
 write_model(model)
