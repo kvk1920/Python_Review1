50a61cc2ff2c53bfb7362855be5fb2ca69bf2a00
kvk1920 <kvk1901@yandex.ru>
2018-03-22 15:54:33 +0300

    Made documentation for function "create_parser"

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
 import sys
 import argparse
+import os
 
 def create_parser():
+    """Create command line parser.
+
+    Create parser what can process all arguments.
+    """
     parser = argparse.ArgumentParser(
         prog="Script for collecting statistics to generation new text.",
         description= '''This program collects statistic on the source texts.''',
@@ -9,9 +14,29 @@ def create_parser():
     )
     parser.add_argument('--input-dir', help= "Path to source directory")
     parser.add_argument('--model', help= 'Path to result directory')
-    parser.add_argument('-lc', help= 'Save texts in lowercase.')
+    parser.add_argument('--lc', help= 'Save texts in lowercase.')
     parser.add_argument('--length', help= 'Length of generation sequence.')
     return parser
 
-cl_parser = create_parser()
-cl_comands = cl_parser.parse_args()
+
+parser = create_parser()
+commands = parser.parse_args()
+
+def get_filelist(commands):
+    filelist = list()
+    filelist.append(sys.stdin)
+
+    if commands.input_dir != None:
+        filenames = os.listdir()
+        filelist.clear()
+        for filename in filenames:
+            filelist.append(open(filename, "r"))
+    return filelist
+
+def process_line(line):
+    '''Process one line.'''
+
+    '''Remove non-alphabetic symbols(and transform to lowercase'''
+
+filelist = get_filelist()
+
