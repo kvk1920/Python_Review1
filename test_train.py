6f5c41fea95e5b0e8b0bb678135f8b27202bf9b6
kvk1920 <kvk1901@yandex.ru>
2018-03-25 15:58:26 +0300

    Fixed test cases bugs

--- test_train.py
+++ test_train.py
@@ -6,31 +6,31 @@ class ParserTestCase(unittest.TestCase):
 
     def test_lc(self):
         parser = train.create_parser()
-        namespace = parser.parse_args("--lc");
+        namespace = parser.parse_args(("--lc", ));
         self.assertIsNotNone(namespace.lc)
 
     def test_input_dir(self):
         parser = train.create_parser()
-        namespace = parser.parse_args("--input-dir input/");
+        namespace = parser.parse_args(("--input-dir", "input/"));
         self.assertEqual("input/", namespace.input_dir)
 
     def test_model(self):
         parser = train.create_parser()
-        namespace = parser.parse_args("--model model.txt")
+        namespace = parser.parse_args(("--mode", "model.txt"))
         self.assertEqual(namespace.model, "model.txt")
 
 
 class PrepareLineTestCase(unittest.TestCase):
 
     def test_lc(self):
-        commands = train.create_parser().parse_args("--lc")
+        commands = train.create_parser().parse_args(("--lc", ))
         words = train.prepare_line("I don't like unittest__oh-oh", commands)
         for word in words:
             self.assertTrue(word.islower())
             self.assertTrue(word.isalpha())
 
     def test_no_lc(self):
-        commands = train.create_parser().parse_args("--model model.txt")
+        commands = train.create_parser().parse_args(("--model", "model.txt"))
         words = train.prepare_line('I hate unittests.,,apple;', commands)
         for word in words:
             self.assertTrue(word.isalpha())
